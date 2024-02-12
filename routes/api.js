e2d656ccfbe90e322ff570b5dd185baf820b2eae
Mauro Walker <mauro@wolfsoft.solutions>
2024-02-12 23:15:40 +0100

    Remove unnecessary imports and functions from api.js + updated the docs

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,5 @@
 // require the needed modules
 var express = require('express');
-var db = require('../db');
-var crypto = require('crypto');
-var { newUser } = require('../functions/email');
-var { send_error } = require('../functions/error');
-var { check_token } = require('../functions/middleware');
-var { rentCar } = require('../functions/cars');
 
 // create the router
 var router = express.Router();
