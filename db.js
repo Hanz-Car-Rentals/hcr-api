d6461cc03e22f1e0f14203effebcda9188548c27
Mauro Walker <mauro@wolfsoft.solutions>
2024-03-04 21:25:06 +0100

    Update .gitignore and package.json, and refactor error.js, docs.js, rentallog.js, and cars.js

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 // require the needed modules
 const mysql = require('mysql');
+var crypto = require('crypto');
 
 // require the config file where all the needed data is stored (db credentials, etc)
 var config = require('./configs/config.json')
@@ -12,71 +13,212 @@ var db = mysql.createConnection({
   database: config.db.name
 });
 
-// create users table if not existing
-db.query('CREATE TABLE IF NOT EXISTS users ( \
+// create the salts table if it doesn't exist. The fields are: id INT AUTOINCREMENT PRIMARY KEY, salt INT NOT NULL
+db.query(`CREATE TABLE IF NOT EXISTS salts (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  salt BLOB NOT NULL
+)`, function (err, result) {
+  if (err) throw err;
+  console.log("Table salts created");
+});
+
+// create the roles table if not exists. the fields are: id INT AUTOINCREMENT PRIMARY KEY, role TEXT NOT NULL, role_desc NOT NULL, role_level INT NOT NULL. The role_level is used to determine the level of the role, the lower the number the higher the level
+db.query("CREATE TABLE IF NOT EXISTS roles ( \
     id INT AUTO_INCREMENT PRIMARY KEY, \
-    email TEXT UNIQUE NOT NULL, \
-    email_verified BOOLEAN DEFAULT false, \
-    email_verify_token TEXT, \
-    token TEXT UNIQUE, \
-    first_name VARCHAR(255), \
-    last_name VARCHAR(255), \
-    password BLOB NOT NULL, \
-    salt BLOB NOT NULL, \
-    password_reset_token TEXT, \
-    reset_password_token_expires_at DATETIME, \
-    admin BOOLEAN DEFAULT 0, \
-    times_rented INT DEFAULT 0, \
-    currently_renting TEXT, \
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, \
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP \
-)');
+    role_name TEXT NOT NULL, \
+    role_desc TEXT NOT NULL, \
+    role_level INT NOT NULL \
+    )", function (err, result) {
+  if (err) throw err;
+  console.log("Table roles created");
+});
+
+// create the following roles in the roles table if the table is empty. These are the roles: Beheerder, Verhuurder, Gebruiker
+db.query("SELECT * FROM roles", function (err, result) {
+  if (err) throw err;
+  // If the roles table is empty, create the default roles
+  if (result.length === 0) {
+      db.query("INSERT INTO roles (role_name, role_desc, role_level) VALUES (?, ?, ?)", ['Beheerder', 'De eigenaar van de website. Zorgt ervoor dat alles goed werkt en grijpt in als er problemen zijn.', 1]);
+      db.query("INSERT INTO roles (role_name, role_desc, role_level) VALUES (?, ?, ?)", ['Voertuigbeheerders', 'Medewerkers die ervoor zorgen dat de auto\'s in orde zijn. Ze regelen reparaties en zorgen dat alle papieren in orde zijn.', 2]);
+      db.query("INSERT INTO roles (role_name, role_desc, role_level) VALUES (?, ?, ?)", ['Verhuurder', 'Medewerkers die beslissen of mensen een auto mogen huren of niet.', 3]);
+      db.query("INSERT INTO roles (role_name, role_desc, role_level) VALUES (?, ?, ?)", ['Gebruiker', 'Mensen die een auto willen huren. Ze zoeken naar een auto, boeken er een en betalen online.', 4]);
+  }
+});
+
+// create the users table if it doesn't exist. The fiels are: id INT NOT NULL PRIMARY KEY AUTOINCREMENT, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, email TEXT NOT NULL, email_verified BOOLEAN, email_verify_token TEXT, token TEXT, password BLOB NOT NULL, salt INT NOT NULL, password_reset_token TEXT, password_reset_token_expires_at TIMESTAMP, role INT NOT NULL, verified_drivers_licence BOOLEAN, times_rented INT, currently_renting INT, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP. The salt is a foreign key to the salts table
+db.query(`
+  CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email TEXT NOT NULL,
+    email_verified BOOLEAN,
+    email_verify_token TEXT,
+    token TEXT,
+    password BLOB NOT NULL,
+    salt INT NOT NULL,
+    password_reset_token TEXT,
+    password_reset_token_expires_at TIMESTAMP,
+    role INT NOT NULL,
+    verified_drivers_licence BOOLEAN,
+    times_rented INT,
+    currently_renting INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (role) REFERENCES roles(id),
+    FOREIGN KEY (salt) REFERENCES salts(id)
+  )
+`, function (err, result) {
+  if (err) throw err;
+  console.log("Table users created");
+});
 
 
-// Create the cars DB if not existing
-db.query("CREATE TABLE IF NOT EXISTS cars ( \
+// add a user to the table if the database is empty
+db.query("SELECT * FROM users", function (err, result) {
+  if (err) throw err;
+
+  // If the users table is empty, create a default user
+  if (result.length === 0) {
+      // Generate a random salt
+      const salt = crypto.randomBytes(16);
+
+      // Hash the password using the salt
+      const password = 'hcr.admin'; // Set your default password here
+      crypto.pbkdf2(password, salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+          if (err) throw err;
+
+          const hashedPasswordHex = hashedPassword.toString('hex'); // Convert hashed password to hexadecimal string
+
+          // Insert the salt into the salts table
+          db.query("INSERT INTO salts (salt) VALUES (?)", [salt], function (err, result) {
+              if (err) throw err;
+
+              // Get the ID of the inserted salt
+              const saltId = result.insertId;
+
+              // Insert the user into the users table with the salt ID and hashed password
+              db.query("INSERT INTO users (first_name, last_name, email, email_verified, password, salt, role) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                  ['Hcr', 'Admin', 'hcr.admin@wolfsoft.solutions', 1, hashedPasswordHex, saltId, 1],
+                  function (err, result) {
+                      if (err) throw err;
+                      console.log("Default user created");
+                  }
+              );
+          });
+      });
+  } else return;
+});
+
+// Create a table called fuel_types where these are the fields: id INT, fuel_type TEXT NOT NULL
+db.query("CREATE TABLE IF NOT EXISTS fuel_types ( \
     id INT AUTO_INCREMENT PRIMARY KEY, \
-    brand VARCHAR(255), \
-    model VARCHAR(255), \
-    seats INT, \
-    space INT, \
-    transmission BOOLEAN, \
-    fuel VARCHAR(255) NOT NULL, \
+    fuel_type TEXT NOT NULL \
+    )", function (err, result) {
+  if (err) throw err;
+  console.log("Table fuel_types created");
+});
+
+// Create a table called body_types where these are the fields: id INT, body_type TEXT NOT NULL
+db.query("CREATE TABLE IF NOT EXISTS body_types ( \
+    id INT AUTO_INCREMENT PRIMARY KEY, \
+    body_type TEXT NOT NULL \
+    )", function (err, result) {
+  if (err) throw err;
+  console.log("Table body_types created");
+});
+
+// Create a table called car_types where these are the fields: id INT, brand TEXT NOT NULL, space INT NOT NULL, fuel INT NOT NULL, doors INT NOT NULL, towing_weight INT, maximum_gross_weight INT NOT NULL, build_year INT NOT NULL, body_type INT NOT NULL. Fuel is a foreign key to the fuel_types table, and body_type is a foreign key to the body_types table
+db.query("CREATE TABLE IF NOT EXISTS car_types ( \
+    id INT AUTO_INCREMENT PRIMARY KEY, \
+    brand TEXT NOT NULL, \
+    space INT NOT NULL, \
+    fuel INT NOT NULL, \
     doors INT NOT NULL, \
-    towing_weight INT NOT NULL, \
-    maximum_gross_weight INT, \
-    location VARCHAR(255) NOT NULL, \
-    picture_url TEXT NOT NULL, \
-    year INT, \
-    color VARCHAR(255), \
-    licence_plate VARCHAR(255), \
-    price DECIMAL(10,2), \
-    rented_by INT, \
-    FOREIGN KEY (rented_by) REFERENCES users(id) \
-)")
+    towing_weight INT, \
+    maximum_gross_weight INT NOT NULL, \
+    build_year INT NOT NULL, \
+    body_type INT NOT NULL, \
+    FOREIGN KEY (fuel) REFERENCES fuel_types(id), \
+    FOREIGN KEY (body_type) REFERENCES body_types(id) \
+    )", function (err, result) {
+  if (err) throw err;
+  console.log("Table car_types created");
+});
 
-// Create the rentallog DB if not existing
-db.query('CREATE TABLE IF NOT EXISTS rentallog ( \
-    id INT AUTO_INCREMENT PRIMARY KEY, \
-    car_id INT, \
-    user_id INT, \
-    rent_date DATE, \
-    return_date DATE, \
-    FOREIGN KEY (car_id) REFERENCES cars(id), \
-    FOREIGN KEY (user_id) REFERENCES users(id) \
-)');
+// Create a table called locations where these are the fields: id INT, location TEXT NOT NULL
+db.query("CREATE TABLE IF NOT EXISTS locations ( \
+  id INT AUTO_INCREMENT PRIMARY KEY, \
+  location TEXT NOT NULL \
+  )", function (err, result) {
+if (err) throw err;
+console.log("Table locations created");
+});
 
-// Create the reviews DB if not existing
-db.query('CREATE TABLE IF NOT EXISTS reviews ( \
+// create the cars table if it doesn't exist the fields are: id INT, car_available BOOLEAN, picture_url TEXT, color VARCHAR, price_per_day DECIMAL, car_type INT, location INT, license_plate VARCHAR, created_at TIMESTAMP, updated_at TIMESTAMP. And the car_type is a foreign key to the car_types table and the location is a foreign key to the locations table
+db.query("CREATE TABLE IF NOT EXISTS cars ( \
+  id INT AUTO_INCREMENT PRIMARY KEY, \
+  car_available BOOLEAN, \
+  picture_url TEXT NOT NULL, \
+  color VARCHAR(255) NOT NULL, \
+  price_per_day DECIMAL(10,2) NOT NULL, \
+  car_type INT NOT NULL, \
+  location INT NOT NULL, \
+  license_plate VARCHAR(255) NOT NULL, \
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, \
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, \
+  FOREIGN KEY (car_type) REFERENCES car_types(id), \
+  FOREIGN KEY (location) REFERENCES locations(id) \
+  )", function (err, result) {
+  if (err) throw err;
+  console.log("Table cars created");
+});
+
+// Create a table called rental_status where these are the fields: id INT, status TEXT NOT NULL
+db.query("CREATE TABLE IF NOT EXISTS rental_status ( \
     id INT AUTO_INCREMENT PRIMARY KEY, \
-    car_id INT, \
-    user_id INT, \
-    review TEXT, \
-    rating INT, \
-    created_at DATE DEFAULT NOW(), \
-    FOREIGN KEY (car_id) REFERENCES cars(id), \
-    FOREIGN KEY (user_id) REFERENCES users(id) \
-)');
+    status TEXT NOT NULL \
+    )", function (err, result) {
+  if (err) throw err;
+  console.log("Table rental_status created");
+});
+
+// Create a table called reviews where these are the fields: id INT, user_id INT, car_id INT, review TEXT NOT NULL, rating INT NOT NULL, created_at TIMESTAMP, updated_at TIMESTAMP. The user_id is a foreign key to the users table and the car_id is a foreign key to the cars table
+db.query("CREATE TABLE IF NOT EXISTS reviews ( \
+    id INT AUTO_INCREMENT PRIMARY KEY, \
+    user_id INT NOT NULL, \
+    car_id INT NOT NULL, \
+    review TEXT NOT NULL, \
+    rating INT NOT NULL, \
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, \
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, \
+    FOREIGN KEY (user_id) REFERENCES users(id), \
+    FOREIGN KEY (car_id) REFERENCES cars(id) \
+    )", function (err, result) {
+  if (err) throw err;
+  console.log("Table reviews created");
+});
+
+
+// Create a table called rentallog where these are the fields: id INT, user_id INT, car_id INT, start_date TIMESTAMP, end_date TIMESTAMP, status INT NOT NULL, created_at TIMESTAMP, updated_at TIMESTAMP. The user_id is a foreign key to the users table and the car_id is a foreign key to the cars table and the status is a foreign key to the rental_status table
+db.query("CREATE TABLE IF NOT EXISTS rentallog ( \
+  id INT AUTO_INCREMENT PRIMARY KEY, \
+  user_id INT NOT NULL, \
+  car_id INT NOT NULL, \
+  start_date TIMESTAMP NOT NULL, \
+  end_date TIMESTAMP NULL DEFAULT NULL, \
+  status INT NOT NULL, \
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, \
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, \
+  FOREIGN KEY (user_id) REFERENCES users(id), \
+  FOREIGN KEY (car_id) REFERENCES cars(id), \
+  FOREIGN KEY (status) REFERENCES rental_status(id) \
+)", function (err, result) {
+  if (err) throw err;
+  console.log("Table rentallog created");
+});
+
+
 
 // export the db connection
 module.exports = db;
--- routes/API/users.js
+++ routes/API/users.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var db = require('../../db');
 var crypto = require('crypto');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 var { newUser, forgot_password } = require('../../functions/email');
 var { send_error } = require('../../functions/error');
 var { check_user_token, admin_check, user_check } = require('../../functions/middleware');
@@ -70,10 +72,9 @@ router.put('/update/:id', check_user_token, user_check, function(req, res, next)
     let first_name = req.body.fname;
     let last_name = req.body.lname;
     let email = req.body.email;
-    let admin = req.body.admin;
 
-    if (first_name && last_name && email && admin) {
-        db.query('UPDATE users SET first_name = ?, last_name = ?, email = ?, admin = ? WHERE id = ?', [first_name, last_name, email, admin, id], function (error, results, fields) {
+    if (first_name && last_name && email && id) {
+        db.query('UPDATE users SET first_name = ?, last_name = ?, email = ?, WHERE id = ?', [first_name, last_name, email, id], function (error, results, fields) {
             if (error) {
                 if(error.code === 'ER_DUP_ENTRY') {
                     res.status(403).send({'status':403, 'message': 'Email already in use'});
@@ -92,42 +93,123 @@ router.put('/update/:id', check_user_token, user_check, function(req, res, next)
     }
 });
 
-router.post('/login', function (req, res) {
-    let email = req.body.email;
-    let password = req.body.password;
-    
-    db.query('SELECT * FROM users WHERE email = ?', [email], function (error, results, fields) {
-        if (error) {
-            send_error(error, "Error logging in");
-            res.send({'status':500, 'message': 'Error logging in'});
-        } else {
-            if (results.length < 1) {
-                res.send({'status':401, 'message': 'Invalid email or password'});
-            } else {
-                let user = results[0];
-                crypto.pbkdf2(password, user.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
-                    if (err) { 
-                        send_error(err, 'Error login');
-                        throw err;
-                    }
-                    if (hashedPassword.equals(user.password)) {
-                        let token = crypto.randomBytes(16).toString('hex');
-                        db.query('UPDATE users SET token = ? WHERE id = ?', [token, user.id], function (error, results, fields) {
-                            if (error) {
-                                send_error(error, "Error logging in");
-                                res.send({'status':500, 'message': 'Error logging in'});
-                            } else {
-                                res.send({'status':200,'message': 'Logged in successfully', 'token': token, 'user': { "admin": user.admin, "id": user.id}});
-                            }
-                        });
-                    } else {
-                        res.send({'status':401, 'message': 'Invalid email or password'});
-                    }
-                });
-            }
+
+// Login route
+router.post('/login', (req, res) => {
+    const { email, password } = req.body;
+  
+    // Check if email and password are provided
+    if (!email || !password) {
+      return res.status(400).json({ message: 'Email and password are required' });
+    }
+  
+    // Query to fetch user details by email
+    const getUserQuery = `SELECT * FROM users WHERE email = ?`;
+  
+    db.query(getUserQuery, [email], (err, results) => {
+      if (err) {
+        console.error('Error executing query:', err);
+        return res.status(500).json({ message: 'Internal server error' });
+      }
+  
+      // Check if user exists
+      if (results.length === 0) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      const user = results[0];
+  
+      // Check if the password is retrieved properly
+      if (!user.password) {
+        return res.status(500).json({ message: 'Password not found for the user' });
+      }
+  
+      // Compare password
+      const storedPassword = user.password.toString().trim(); // Convert to string and trim
+      const providedPassword = password.toString().trim(); // Convert to string and trim
+      
+      // Hash provided password before comparison
+      const providedPasswordHash = bcrypt.hashSync(providedPassword, 10);
+      
+      bcrypt.compare(providedPasswordHash, storedPassword, (err, isMatch) => {
+        if (err) {
+          console.error('Error comparing passwords:', err);
+          return res.status(500).json({ message: 'Internal server error' });
         }
+  
+        if (!isMatch) {
+          return res.status(401).json({ message: 'Invalid password' });
+        }
+  
+        // Generate JWT token
+        const token = jwt.sign({ userId: user.id }, 'your_secret_key', { expiresIn: '1h' });
+  
+        // Return user data and token
+        res.status(200).json({
+          user: {
+            id: user.id,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            email: user.email
+            // Add other fields as needed
+          },
+          token
+        });
+      });
     });
-});
+  });
+
+
+
+// router.post('/login', function (req, res) {
+//     let email = req.body.email;
+//     let password = req.body.password;
+
+//     db.query('SELECT * FROM users WHERE email = ?', [email], function (error, results, fields) {
+//         if (error) {
+//             send_error(error, "Error logging in");
+//             res.send({'status':500, 'message': 'Error logging in'});
+//         } else {
+//             if (results.length < 1) {
+//                 res.send({'status':401, 'message': 'Invalid email or password'});
+//             } else {
+//                 // get the salt from the salts table where the user_id is the users id.
+//                 let user = results[0];
+//                 db.query("SELECT salt FROM users WHERE id = ?", [user.salt], function (error, results, fields) {
+//                     if(error) throw error;
+//                     let salt = results[0].salt;
+//                     crypto.pbkdf2(password, salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+//                         if (err) { 
+//                             send_error(err, 'Error login');
+//                             throw err;
+//                         }
+//                         if (hashedPassword.equals(user.password)) {
+//                             let token = crypto.randomBytes(16).toString('hex');
+//                             db.query('UPDATE users SET token = ? WHERE id = ?', [token, user.id], function (error, results, fields) {
+//                                 if (error) {
+//                                     send_error(error, "Error logging in");
+//                                     res.send({'status':500, 'message': 'Error logging in'});
+//                                 } else {
+//                                     // get the role_name from the roles table where the role.id = user.role
+//                                     db.query('SELECT role_name FROM roles WHERE id = ?', [user.role], function (error, results, fields) {
+//                                         if (error) {
+//                                             send_error(error, "Error logging in");
+//                                             res.send({'status':500, 'message': 'Error logging in'});
+//                                         }
+//                                         let role = results[0].role_name;
+//                                         res.send({'status':200,'message': 'Logged in successfully', 'token': token, 'user': { "role": role, "id": user.id}});
+//                                     });
+//                                 }
+//                             });
+//                         } else {
+//                             res.send({'status':401, 'message': 'Invalid email or password'});
+//                         }
+//                     });
+//                 });
+//             }
+//         }
+//     });
+// });
 
 router.get('/verify/:token', function(req, res, next){
     db.query('SELECT * FROM users WHERE email_verify_token = ?', [req.params.token], function(err, rows) {
